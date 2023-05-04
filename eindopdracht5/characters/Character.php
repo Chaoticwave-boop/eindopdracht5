359c71e1e8f31c57840be1d690c024ead97b4d2a
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-05-04 16:01:59 +0200

    navbar charachter show fixed

--- eindopdracht5/characters/Character.php
+++ eindopdracht5/characters/Character.php
@@ -1,43 +1,43 @@
 <?php 
-	$people = getInfo();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php foreach($people as $person){ ?>
-        <h1><?= $person['name'] ?></h1>
-		<p><?= $person['bio'] ?></p>
-        <img src=images/<?= $person['avatar']?> alt="ellie"  >
-		
-    <?php } ?>
-
-</body>
-</html>
-
-<?php
-function getInfo(){
-    // maak een verbinding
+    function getInfo(){
+        $CurrentCharacter = $_GET["character"];
         $connection = connect();
 
-    // maak een query
-        $query = "SELECT * FROM `characters`";
+        $query = "SELECT * FROM characters Where name = '$CurrentCharacter'";
 
-    // voorbereid een query
         $statement = $connection->prepare($query);
 
-    // voer de query uit
         $statement->execute();
 
-    // haal de result op
         $result = $statement->fetchALl();
 
         return $result;
+    }
+
+    if(array_key_exists("character", $_GET ) ) { 
+        $people = getInfo();
+       
+        ?>
+
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <?php foreach($people as $person){ ?>
+                <h1><?= $person['name'] ?></h1>
+                <p><?= $person['bio'] ?></p>
+                <img src=images/<?= $person['avatar']?> >
+
+            <?php } ?>
+
+        </body>
+        </html>
+
+        <?php
     }
 ?>
--- eindopdracht5/includes/header.php
+++ eindopdracht5/includes/header.php
@@ -7,26 +7,16 @@
     <!-- Side navigation -->
     <div class="sidenav">
         <?php foreach($people as $person){ ?>
-            <a href="index.php?name=Character" id="headerbutton"><?= $person['name'] ?></a>
+            <a href="index.php?name=Character&character=<?= $person['name']?>" id="headerbutton"><?= $person['name'] ?></a>
         <?php } ?>
     </div>
         
     <?php
         function getPeople(){
-
-        // maak een verbinding
             $connection = connect();
-
-        // maak een query
             $query = "SELECT * FROM `characters`";
-
-        // voorbereid een query
             $statement = $connection->prepare($query);
-
-        // voer de query uit
             $statement->execute();
-
-        // haal de result op
             $result = $statement->fetchALl();
 
             return $result;
